211c6ad2039f86da183f98431fa98164d2b6960d
mohem1200 <112038023+mohem1200@users.noreply.github.com>
2024-04-27 14:29:46 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Handling form submission
     var form = document.getElementById('booking-form');
-    if (form) {
-        form.addEventListener('submit', function(event) {
-            event.preventDefault();
-            const team = document.getElementById('team').value;
-            const date = document.getElementById('date').value;
-            const ticketsNumber = document.getElementById('tickets').value;
-            alert(`You have successfully booked ${ticketsNumber} tickets for ${team} on ${date}.`);
-        });
-    }
-
-    // Example navigation button functionality
-    document.querySelectorAll('nav a').forEach(function(link) {
-        link.addEventListener('click', function(event) {
-            event.preventDefault();
-            var targetId = this.getAttribute('href').substring(1); // Get the target id (removing '#')
-            var targetElement = document.getElementById(targetId);
-            if (targetElement) {
-                targetElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
-            }
-        });
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        const team = document.getElementById('team').value;
+        const date = document.getElementById('date').value;
+        const ticketsNumber = document.getElementById('tickets').value;
+        alert(`You have successfully booked ${ticketsNumber} tickets for ${team} on ${date}.`);
     });
 });
